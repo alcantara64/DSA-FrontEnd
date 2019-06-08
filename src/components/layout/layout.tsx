a06c1d25b15dc6d0e6788c8ea61b325f05f5934d
Giwa Ayo <ugiwa@eminenttechnology.com>
2019-06-08 14:09:39 +0100

    Add styling and unity css

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Auxi from '../../hoc/Auxi';
+
+const layout = () => (
+<Auxi>
+    <div>Header</div>
+    <main>
+        Components
+    </main>
+    <div>footer</div>
+</Auxi>
+)
+
+export default layout;
