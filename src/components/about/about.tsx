ab16063cc4577809bf02be778dc74073a6c7333d
Giwa Ayo <ugiwa@eminenttechnology.com>
2019-06-14 19:29:27 +0100

    Update about page

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Auxi from '../../hoc/Auxi';
 import './about.css';
 import Footer from '../app-footer/footer';
+import Find_Solutions_Illustration from './../../asset/images/Find_Solutions_Illustration.png';
+
 
 const about = () => {
 return (
@@ -13,11 +15,11 @@ return (
         <div className="custom-paragraph custom-paragraph-width">We accelerate high-value, actionable insights through the delivery of scalable, enterprise data platforms, data science and analytics solutions.</div>
         </div>
 
-        <div className="em-u-text-align-left em-l em-l--two-column-wide custom-line-spacing">
-  <div className="em-l__main">
+        <div className="em-l em-l--two-column-wide custom-mid-section-area">
+  <div className="em-l__main custom-main-area">
 
-            <div className="em-l-grid custom-paragraph-margin em-l-grid--1-2-1 em-l-grid--break-fast">
-            <div className="em-l-grid__item">
+            <div className="em-l-grid em-u-text-align-left em-l-grid--1-2-1 em-l-grid--break-fast">
+            <div className="em-l-grid__item em-u-padding-top">
                 
         <div className="custom-H3">Who are we?</div>
         
@@ -26,7 +28,7 @@ return (
         </div>
 
             </div>
-            <div className="em-l-grid__item">
+            <div className="em-l-grid__item em-u-padding-right-none">
                 
 
             <div className="custom-H3">Who can we do for you?</div>
@@ -37,50 +39,157 @@ return (
         
         
         <div className="row custom-line-spacing">
-            <div className="column em-u-margin-left-none">
+            <div className="column custom-right-tile-margin em-u-margin-left-none">
                 <div className="custom-paragraph">
                     We were able to level set from across the business and technical folks, and then walk out of a day and a half engagement
                     with a working prototype of a visualization with mock data
             </div>
-            <div className="custom-paragraph custom-line-spacing">
-                <span className="custom-paragraph text-underline">Nate McIntire, C&D AND FTP VIZ LEAD</span>
+            <div className="custom-link custom-line-spacing em-u-margin-top">
+                <span className="custom-link ">Nate McIntire, C&D AND FTP VIZ LEAD</span>
             </div>
 
+<div className="custom-small-tile-underline">
+
+</div>
 
             </div>
-            <div className="column">
+            <div className="column custom-right-tile-margin custom-left-tile-margin">
                 <div className="custom-paragraph">
                     This design thinking workshop with data makes alot of progress and is an impressive team.
             </div>
-            <div className="custom-paragraph custom-line-spacing">
-                <span className="custom-paragraph text-underline">Nick Smith, MS-BMR</span>
+            <div className="custom-link custom-line-spacing em-u-margin-top">
+                <span className="custom-link ">Nick Smith, MS-BMR</span>
             </div>
+
+            
+<div className="custom-small-tile-underline">
+    
+    </div>
+
             </div>
             
             <div className="column">
-                <div className="custom-paragraph">
-We can save alot of time just making a few small changes 
+                <div className="custom-paragraph">We can save a lot of time just making a few small changes through data and analytics in this process.
             </div>
-            <div className="custom-paragraph custom-line-spacing">
-                <span className="custom-paragraph text-underline">Nick Smith, MS-BMR</span>
+            <div className="custom-link custom-line-spacing em-u-margin-top">
+                <span className="custom-link ">Jeremy Janicki, FTP</span>
             </div>
 
 
+            <div className="custom-small-tile-underline">
+    
+    </div>
+
             </div>
-            <div className="column">
-</div>
-<div className="column">
-3
-</div>
         </div>
 
 
             </div>
             </div>
 
+            <div className="em-l-grid__item em-u-text-align-left">
+                
+                <div className="custom-H3">How should you engage our services?</div>
+                
+                <div className="custom-paragraph">Use the Services page to find the contact, resources, or external link you’re looking for. We know you’d rather deal with a person than a form, so we’ve tried our best to provide the contacts that will be most helpful.                </div>
+        
+                    </div>
+
+            <div className="em-l-grid__item custom-search-button-margin em-u-text-align-left">
+            <div className="custom-paragraph em-u-margin-bottom">Have another question you don’t see here? Send it our way.
+        </div>
+
+
+        <form method="post" action="#" role="search" className="em-c-search-form em-js-header-search">
+  <div className="em-c-search__body">
+    <input type="search" id="search" className="em-c-search__input" />
+    <button className="em-c-btn custom-button-color em-c-btn--primary" aria-label="Search">
+      <div className="em-c-btn__inner">
+      </div>
+    </button>
   </div>
-  <div className="em-l__secondary">
-    <div className="fpo-block">Secondary</div>
+</form>
+
+            </div>
+  </div>
+  <div className="em-l__secondary em-u-text-align-left custom-secondary-area em-u-padding">
+  <div className="custom-H3 ">Our core value</div>
+  <div className="custom-secondary-tile-detail-area em-u-margin-top">
+      <div className="custom-factory-img"></div>
+      <div className="custom-H4 no-text-transform">Enterprise Platform</div>
+      <div className="custom-paragraph">Assess market for new and emerging technologies and evaluate for suitability for near-term use cases (SAP VORA).</div>
+  </div>
+
+  
+  <div className="custom-secondary-tile-detail-area em-u-margin-top">
+      <div className="custom-factory-img"></div>
+      <div className="custom-H4 no-text-transform">Technical Expertise</div>
+      <div className="custom-paragraph">Maintain and support a diverse platform of interconnected data and analytics technolgies.</div>
+  </div>
+
+  
+  <div className="custom-secondary-tile-detail-area em-u-margin-top">
+      <div className="custom-factory-img"></div>
+      <div className="custom-H4 no-text-transform">Corporate Breadth</div>
+      <div className="custom-paragraph">Develop data & analytics technology strategies for the corporation (technical roadmaps, reference architecture).</div>
+  </div>
+
+  
+  <div className="custom-secondary-tile-detail-area em-u-margin-top">
+      <div className="custom-factory-img"></div>
+      <div className="custom-H4 no-text-transform">E2E Solutions</div>
+      <div className="custom-paragraph">Maintain strategic partnerships to understand products in the marketplace and to maximize value from existing technolgies.</div>
+  </div>
+  
+  </div>
+</div>
+
+            <div className="custom-mid-background-area">
+
+                    <div className="row custom-header-margin-left">
+                        <div className="custom-mid-page-slider-photo-column">
+                        <img src={Find_Solutions_Illustration} alt="" className="custom-mid-photo-area" />
+
+                        </div>
+
+                        
+                            <div className="custom-mid-page-slider-details-column purple-text-color">
+                                
+                        <div className="em-u-text-align-left">
+                        <span className="dot"></span>
+                            <span className="dot"></span>
+                            <span className="dot"></span>
+                            <span className="dot"></span>
+                        </div>
+                        
+                        <div className="custom-link purple-text-color no-text-transform custom-mid-page-header-margin">TESTIMONIALS</div>
+
+                        <div className="custom-about-regular-h3 custom-margin">We were able to level set from across the business and technical folks, and then walk out of a 1.5 day engagement with a working prototype of a visualization with mock data.</div>
+
+
+                        <div className="custom-paragraph purple-text-color custom-margin">Nate McIntire, C&D AND FTP VIZ LEAD</div>
+                        </div>
+                    </div>
+
+            </div>
+
+
+
+            <div className="em-l-grid em-l-grid--5up ">
+  <div className="em-l-grid__item custom-mid-page-card-column">
+    <div className="fpo-block">1</div>
+  </div>
+  <div className="em-l-grid__item custom-mid-page-card-column">
+    <div className="fpo-block fpo-block--dark">2</div>
+  </div>
+  <div className="em-l-grid__item custom-mid-page-card-column">
+    <div className="fpo-block">3</div>
+  </div>
+  <div className="em-l-grid__item custom-mid-page-card-column">
+    <div className="fpo-block fpo-block--dark">4</div>
+  </div>
+  <div className="em-l-grid__item custom-mid-page-card-column">
+    <div className="fpo-block">5</div>
   </div>
 </div>
 
