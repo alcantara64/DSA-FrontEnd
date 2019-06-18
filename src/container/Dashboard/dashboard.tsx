d19cc527c951f87714b9ee05754b787483cdad65
Giwa Ayo <ugiwa@eminenttechnology.com>
2019-06-18 15:57:37 +0100

    fix bug

--- src/container/Dashboard/dashboard.tsx
+++ src/container/Dashboard/dashboard.tsx
@@ -5,9 +5,16 @@ import About from '../../components/about/about';
 import Blog from '../../components/blog/blog';
 import { Switch, Route } from 'react-router';
 import Auxi from '../../hoc/Auxi'
+import axios from 'axios';
 
 
 class dashboard extends Component {
+
+    componentDidMount() {
+        return axios.get('http://localhost:52770/archive?id=d078727c-6943-4c62-8943-3fdd3f3acdf0').then(
+            (rsp) => console.log(rsp)
+        );
+    }
 
     //class should container method reference for header routing and footer visiblity
     //state sould only change in component
