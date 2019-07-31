8ed774909731550c3d62321e74529b19ee3f9662
Ebhoaye Lucky <lebhoaye@eminenttechnology.com>
2019-07-31 19:41:42 +0100

    Fix bugs

--- src/components/services/serviceoptions/serviceOptions.tsx
+++ src/components/services/serviceoptions/serviceOptions.tsx
@@ -45,7 +45,7 @@ export default class ServiceOptions extends Component<IServiceOptionsProps, ISer
 
                 {this.state.showLabel? <div className=" large-flex custom-btn-container custom--btn-options" onClick={() => this.showSelectedOptions(this.state.optionText, this.props.label)} >{this.state.optionText}<img className="information-white-icon" src={informationWhite} alt=""/></div>
                 : this.props.options.map((opt) => {
-                    return (<div className="custom-btn-contain custom-btn-align">
+                    return (<div className="custom-btn-contain large-flex custom-btn-align">
                     <button key={opt.optionCode} className="custom-service-btn" 
                     onClick={() => this.showSelectedOptions(opt.optionText, this.props.label)} >{opt.optionText} 
                     <img className="information-icon" src={information} alt=""/>
